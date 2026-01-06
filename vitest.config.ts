35103cc18f36699980743807f2ff60040efd9868
Artem Antonov <antonov.artem@gmail.com>
2026-01-05 19:12:14 -0800

    Update vitest.config.ts

--- vitest.config.ts
+++ vitest.config.ts
@@ -2,19 +2,19 @@ import { defineConfig } from "vitest/config";
 
 export default defineConfig({
   test: {
-    globals: true,
-    environment: "node",
     coverage: {
+      exclude: ["node_modules/", "dist/", "test/", "src/index.ts"],
       provider: "v8",
       reporter: ["text", "json", "html"],
-      exclude: ["node_modules/", "dist/", "test/", "src/index.ts"],
       thresholds: {
-        lines: 80,
-        functions: 80,
         branches: 80,
+        functions: 80,
+        lines: 80,
         statements: 80,
       },
     },
+    environment: "node",
+    globals: true,
     setupFiles: ["./test/setup.ts"],
   },
 });
